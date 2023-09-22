93bcd976f1e1d6c8f9b2c780ad4d02d4e6f7e36e
nik afz <nikafz980@gmail.com>
2023-09-22 14:02:42 +0400

    added all files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useRef } from "react";
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <video
+        src="https://dl4.freeverss.online/Series/1400/Invasion/S01/480p/Invasion.2021.S01E01.480p.WEB-DL.x264-Pahe.SoftSub.AvaMovie.mkv"
+        width={1060}
+        height={540}
+        controls
+      />
+    </>
+  )
+}
+
+export default App
