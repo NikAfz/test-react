ee287a8f08eb77d77a3299de4722f71e0ab5c0ed
nik afz <nikafz980@gmail.com>
2023-09-22 16:50:09 +0400

    test-1

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,17 @@ function App() {
 
   return (
     <>
-      <video
+      {/* <video
         src="https://dl4.freeverss.online/Series/1400/Invasion/S01/480p/Invasion.2021.S01E01.480p.WEB-DL.x264-Pahe.SoftSub.AvaMovie.mkv"
         width={1060}
         height={540}
+        controls
+      /> */}
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam facere tenetur inventore obcaecati, itaque laudantium necessitatibus rem! Rem, quis totam eaque quaerat ducimus alias at dolore praesentium quo maiores architecto!</p>
+      <video
+        src="https://dl5.freeverss.online/Film5/1399/10/Blade.Runner.2049.2017/Blade.Runner.2049.2017.1080p.Ganool.SoftSub.AvaMovie.mkv"
+        width={1060}
+        
         controls
       />
     </>
